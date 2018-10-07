2ec83204bd65ee0e66c319ab38ff1c83b3d58fdc
Nahian Bin Aziz <nahianbinaziz@Trevor.hitronhub.home>
2018-10-07 14:15:37 -0700

    Email Password Facebook Google

---
+++ src/app/config/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDCWvT52mHRUvq92aZC1-0y7a4g6Ofk8q0',
+  authDomain: 're-vent.firebaseapp.com',
+  databaseURL: 'https://re-vent.firebaseio.com',
+  projectId: 're-vent',
+  storageBucket: '',
+  messagingSenderId: '380201963986'
+};
+
+firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore();
+const settings = {
+  timestampsInSnapshots: true
+};
+firestore.settings(settings);
+
+export default firebase;
--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const HomePage = ({history}) => {
+const HomePage = ({ history }) => {
   return (
     <div>
       <div className="ui inverted vertical masthead center aligned segment">
@@ -14,27 +14,19 @@ const HomePage = ({history}) => {
             <div className="content">Re-vents</div>
           </h1>
           <h2>Do whatever you want to do</h2>
-          <div onClick={() => history.push('/events')} className="ui huge white inverted button">
+          <div
+            onClick={() => history.push('/events')}
+            className="ui huge white inverted button"
+          >
             Get Started
             <i className="right arrow icon" />
           </div>
         </div>
       </div>
       <div style={{ textAlign: 'center' }}>
-        Icons made by{' '}
-        <a href="http://www.freepik.com" title="Freepik">
-          Freepik
-        </a>{' '}
-        from{' '}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>{' '}
-        is licensed by{' '}
-        <a
-          href="http://creativecommons.org/licenses/by/3.0/"
-          title="Creative Commons BY 3.0"
-        >
-          CC 3.0 BY
+        <strong>Created by </strong>
+        <a href="https://github.com/Nahian-Aziz" title="Github">
+          <strong>Nahian Aziz</strong>
         </a>
       </div>
     </div>
